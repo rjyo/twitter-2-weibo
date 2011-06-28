5189f3d4d2d642b71632cfc7a182441ed6db1606
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2011-06-28 18:33:50 +0900

    support multi modules

---
+++ worker/src/main/java/h2weibo/worker/App.java
@@ -0,0 +1,13 @@
+package h2weibo.worker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
